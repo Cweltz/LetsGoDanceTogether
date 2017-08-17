4ab9922b509a26646893a08258190967a09edb45
Cweltz <christopher@weltz-erbach.de>
2017-08-17 11:00:03 +0200

    Added a test comment.

--- LetsGoDanceTogether/ViewController.swift
+++ LetsGoDanceTogether/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Added a test comment
         // Dispose of any resources that can be recreated.
     }
 
